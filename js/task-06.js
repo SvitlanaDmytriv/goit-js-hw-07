5ccf3c76e0ffde4271b03e95b55cdc879ec470e9
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2021-07-13 10:47:49 +0300

    done-06+

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,9 @@
 const inputEl = document.querySelector('#validation-input')
 
+
 inputEl.onblur = function () {
     console.log(this.value.length);
-    if (this.getAttribute('data-length') > this.value.length || this.getAttribute('data-length') < this.value.length) {
+    if (Number(this.getAttribute('data-length')) !== this.value.length ) {
     this.classList.add('invalid');
   } else {
     this.classList.remove('invalid');
